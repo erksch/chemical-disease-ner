bcc4d5cc1c55154e9682155624f783baca9e9466
Erik Ziegler <erik.ziegler@student.uni-potsdam.hpi.de>
2020-07-08 17:04:20 +0200

    Improve evaluation.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from torch.utils.tensorboard import SummaryWriter
 from utils import process_dataset_xml, format_to_tensor, prepare_embeddings
 from model import BiLSTM
 
 def main():
   print(f"Torch Version: {torch.__version__}")
   print(f"CUDA available: {torch.cuda.is_available()}")
+
+  writer = SummaryWriter()
 
   print("Preprocessing data...")
   train_sentences = process_dataset_xml('data/CDR_Data/CDR.Corpus.v010516/CDR_TrainingSet.BioC.xml')
@@ -22,12 +25,32 @@ def main():
 
   model = BiLSTM(word_embeddings=torch.FloatTensor(word_embeddings), num_classes=len(label2Idx))
 
-  epochs = 50
+  epochs = 100
   learning_rate = 0.015
   momentum = 0.9
 
   criterion = nn.CrossEntropyLoss()
   optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+  n_iter = 0
+
+  def predict_dataset(dataset, net):
+    all_true_labels = []
+    all_predicted_labels = []
+
+    for tokens, true_labels in dataset:
+        tokens = torch.LongTensor([tokens])
+        true_labels = torch.LongTensor(true_labels)
+
+        predicted_labels = net(tokens)
+        predicted_labels = predicted_labels.argmax(axis=2).squeeze(dim=0)
+
+        for i in range(len(true_labels)):
+            all_true_labels.append(true_labels[i].item())
+            all_predicted_labels.append(predicted_labels[i].item())
+
+    return torch.LongTensor(all_true_labels), torch.LongTensor(all_predicted_labels)
+
+  print("Training...")
 
   for epoch in range(epochs):  
         model.train()
@@ -42,36 +65,38 @@ def main():
             loss.backward()
             optimizer.step()
 
-        print(f"Epoch {epoch}, Loss {loss.item()}")
+            writer.add_scalar('Loss/train_step', loss, n_iter)
+            n_iter += 1
+        
 
         model.eval()
 
-        correct = 0
-        total = 0
-        classes_correct = {}
-        classes_total = {}
+        ground_truth, predictions = predict_dataset(train_sentences, model)
+        true_positives = (ground_truth == predictions).sum().item()
+        accuracy = true_positives / len(ground_truth)
+        writer.add_scalar('Accuracy/All/train_epoch', accuracy, epoch) 
 
-        for tokens, true_labels in train_sentences:
-            tokens = torch.LongTensor([tokens])
-            true_labels = torch.LongTensor(true_labels)
-            predicted_labels = model(tokens)
-            predicted_labels = predicted_labels.argmax(axis=2).squeeze(dim=0)
-            total += len(predicted_labels)
-            correct += (predicted_labels == true_labels).sum().item()
+        print(f"Epoch {epoch}, Loss {loss.item()}, Accuracy {accuracy}")
+        
+        for label in idx2Label.keys():
+            indices_in_class = torch.where(ground_truth == label)[0]
+            true_positives = (ground_truth[indices_in_class] == predictions[indices_in_class]).sum().item()
+            false_negatives = len(indices_in_class) - true_positives
+            
+            recall = true_positives / len(indices_in_class)
+            
+            indices_predicted_in_class = torch.where(predictions == label)[0]
+            false_positives = (ground_truth[indices_predicted_in_class] != predictions[indices_predicted_in_class]).sum().item()
 
-            for i, true_label in enumerate(true_labels):
-                true_label = true_label.item()
-                if not true_label in classes_correct:
-                    classes_correct[true_label] = 0
-                    classes_total[true_label] = 0
-                classes_total[true_label] += 1 
-                if true_label == predicted_labels[i].item():
-                    classes_correct[true_label] += 1
+            precision = true_positives / (true_positives + false_positives)
 
-        print(f"All classes: {correct} / {total}, Accuracy {correct / total}")
+            f1_score = (2 * true_positives) / (2 * true_positives + false_positives + false_negatives)
 
-        for label_idx in list(classes_total.keys()):
-            print(f"{idx2Label[label_idx]}: {classes_correct[label_idx]} / {classes_total[label_idx]}, Accuracy {classes_correct[label_idx] / classes_total[label_idx]}")    
+            print(f"{idx2Label[label]}: Precision {precision}, Recall {recall}, F1 Score {f1_score}")
 
+            writer.add_scalar(f"Precision/{idx2Label[label]}/train_epoch", precision, epoch)
+            writer.add_scalar(f"Recall/{idx2Label[label]}/train_epoch", recall, epoch)
+            writer.add_scalar(f"F1Score/{idx2Label[label]}/train_epoch", f1_score, epoch)
+       
 if __name__ == '__main__':
     main()
