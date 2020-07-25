1179a74dd1f4d0ac582457da7ea94c24443d7d7a
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2020-07-25 13:19:47 +0200

    First draft for char inputs.

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ torch.manual_seed(1)
 
 class BiLSTM(nn.Module):
     
-    def __init__(self, CONFIG, vocab_size, num_classes, batch_size, word_embeddings=None):
+    def __init__(self, CONFIG, vocab_size, num_classes, num_chars, batch_size, word_embeddings=None):
         super(BiLSTM, self).__init__()
         
         self.batch_size = batch_size
@@ -15,12 +15,16 @@ class BiLSTM(nn.Module):
 
         self.hidden_dim = CONFIG['hidden_dim']
 
+        self.char_embedding = nn.Embedding(num_chars, 30)
+        self.conv1d = nn.Conv1d(30, 128, kernel_size=1, stride=1)
+        self.maxpool = nn.MaxPool1d(kernel_size=2, stride=2)
+
         if CONFIG['use_pretrained_embeddings']:            
-            self.embedding_dim = word_embeddings.shape[1] 
-            self.embedding = nn.Embedding.from_pretrained(word_embeddings)
+            self.word_embedding_dim = word_embeddings.shape[1] 
+            self.word_embedding = nn.Embedding.from_pretrained(word_embeddings)
         else:
-            self.embedding_dim = CONFIG['embeddings_dim']
-            self.embedding = nn.Embedding(vocab_size, self.embedding_dim)
+            self.word_embedding_dim = CONFIG['embeddings_dim']
+            self.word_embedding = nn.Embedding(vocab_size, self.embedding_dim)
 
         self.dropout = nn.Dropout(CONFIG['dropout'])
         self.lstm = nn.LSTM(self.embedding_dim, self.hidden_dim, bidirectional=True, batch_first=True)
@@ -37,10 +41,19 @@ class BiLSTM(nn.Module):
         return (torch.randn(2, batch_size, self.hidden_dim).to('cuda'),
                 torch.randn(2, batch_size, self.hidden_dim).to('cuda'))
 
-    def forward(self, x):
-        self.hidden = self._init_hidden(x.shape[0])
-        x = self.embedding(x)
-        x, self.hidden = self.lstm(x, self.hidden)
+        self.hidden = self._init_hidden(xt.shape[0])
+
+        # char input
+        xc = self.char_embedding(xc)    # (B, N, 30)
+        xc = F.relu(self.conv1d(xc))    # (B, N, 128)
+        xc = self.maxpool(xc)           # (B, N, 64)
+
+        # word / token input
+        xt = self.word_embedding(xt)    # (B, N, E)
+
+        x = torch.cat([xt, xc])         # (B, N, E + 64)
+
+        x, self.hidden = self.lstm(x, self.hidden)             # (B, N, 2*H)
 
         if self.use_dropout:
             x = self.dropout(x)
